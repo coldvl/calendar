31f6a31c1e9755398080c00a73dea2060659e261
coldvl <78581245+coldvl@users.noreply.github.com>
2021-02-14 15:47:57 +0200

    Update entry.js

--- src/entry.js
+++ src/entry.js
@@ -1,1 +1,10 @@
+import { renderCreateEventPage } from './create-event-page.js';
+import { renderCalendarPage } from './calendar-page.js';
 
+window.addEventListener("load", () => {
+  const redirectToCalendar = () => {
+    renderCalendarPage(renderCreateEventPage, redirectToCalendar)
+  }
+  
+  renderCalendarPage(renderCreateEventPage, redirectToCalendar)
+})
