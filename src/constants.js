a8727a8c6450f28d0f9612da97c4d323296db8ab
coldvl <78581245+coldvl@users.noreply.github.com>
2021-02-14 15:45:57 +0200

    Create constants.js

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const eventList = [
+  {
+    "eventType": "Daily Standup",
+    "day": "mon",
+    "time": "11",
+    "participants": ["Bob"]
+  },
+  {
+    "eventType": "Planning Session",
+    "day": "wed",
+    "time": "13",
+    "participants": ["Marry"]
+  },
+  {
+    "eventType": "Retrospective",
+    "day": "fri",
+    "time": "17",
+    "participants": ["Marry", "Bob"]
+  }
+]
