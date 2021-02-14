924029048e67a897ee94e052e4d6b0762b287723
coldvl <78581245+coldvl@users.noreply.github.com>
2021-02-14 15:47:25 +0200

    Update create-event-page.js

--- src/create-event-page.js
+++ src/create-event-page.js
@@ -1,1 +1,50 @@
+import { eventList } from './constants.js'
 
+const subscribeToCreatePageEvents = (redirectToCalendar) => {
+  const cancelEventFunction = (e) => {
+    e.preventDefault();
+    redirectToCalendar()
+  }
+
+  const createEventFunction = (e) => {
+    e.preventDefault();
+    const eventForm = document.getElementById("create-event-form")
+    const formData = new FormData(eventForm)
+
+    const jsonData = Object.fromEntries(formData.entries());
+    jsonData.participants = formData.getAll('participants');
+
+    const existing_event = eventList.find((listItem) => {
+      return listItem.day === jsonData.day && listItem.time === jsonData.time
+    })
+
+    if (!existing_event) {
+      eventList.push(jsonData);
+      redirectToCalendar()
+    } else {
+      alert('Event is already existing')
+    }
+  }
+
+  const submitButton = document.getElementById("event-form-submit")
+  submitButton.onclick = createEventFunction;
+
+  const cancelButton = document.getElementById("event-form-cancel")
+  cancelButton.onclick = cancelEventFunction;
+}
+
+export const renderCreateEventPage = (redirectToCalendar) => {
+  fetch('create-event-template.html').then(
+    (response) => {
+      return response.text()
+    }
+  ).then(
+    (createEventTemplate) => {
+      const container = document.getElementById('content-container')
+      window.history.replaceState({}, 'Create Event', 'create-event.html');
+      container.innerHTML = createEventTemplate
+      $('.selectpicker').selectpicker();
+      subscribeToCreatePageEvents(redirectToCalendar)
+    }
+  )
+}
